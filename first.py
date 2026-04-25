6168478788373f14d868311a93d5ebc48a69df90
iamshubham2001 <iamshubh2001@gmail.com>
2026-04-25 21:51:07 +0530

    hi

---
+++ first.py
@@ -0,0 +1,34 @@
+# print ("Hello!")
+
+# # a python module is a file containing python code which can be imported and used in other python files. A module can contain functions, classes, and variables. A module can also include runnable code.   
+# # piu is a python module which contains functions and classes for performing various mathematical operations. It is a built-in module in python and can be imported using the import statement.
+# import math
+
+# print ('shubh' + 'am') # String concatenation - joining two strings together
+# print (2**2) # Exponentiation - raising a number to the power of another number
+# print (math.sqrt(16)) # Square root - finding the square root of a number using the math module
+
+'''
+my_name = input('>') # Taking input from the user and storing it in a variable
+print (f"Hello {my_name}!") # Using f-string to print the value of the variable in a string 
+print(f'it is good to meet you,' + my_name) # String concatenation - joining a string and a variable together 
+print(len(my_name)) # len() - to find the length of the string stored in the variable
+print(my_name.upper()) # upper() - to convert the string stored in the variable to uppercase
+print(my_name.lower()) # lower() - to convert the string stored in the variable to lowercase
+print(my_name.title()) # title() - to convert the string stored in the variable to title
+print(my_name.strip()) # strip() - to remove any leading or trailing whitespace from the string stored in the variable
+print('what is your age?') # print() - to print a string to the console
+my_age = input('How old are you? ') # Taking input from the user and storing it in a variable
+print('you will be ' + str(int(my_age) + 1) + ' years old next year') # str() - to convert a number to a string, int() - to convert a string to a number
+'''
+
+'''
+type (43)
+<class 'int'>
+type (3.14)
+<class 'float'>
+type ("Hello")
+<class 'str'>
+type (True)
+<class 'bool'>
+'''
